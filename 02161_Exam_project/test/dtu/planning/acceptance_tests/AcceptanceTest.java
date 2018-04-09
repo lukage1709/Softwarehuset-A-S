035781b50d54ffa90e7eb8d08303c1f78e6873c1
s144025 <9057612+s144025@users.noreply.github.com>
2018-04-09 12:27:50 +0200

    updated

--- 02161_Exam_project/test/dtu/planning/acceptance_tests/AcceptanceTest.java
+++ 02161_Exam_project/test/dtu/planning/acceptance_tests/AcceptanceTest.java
@@ -1,5 +1,17 @@
 package dtu.planning.acceptance_tests;
 
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "use_cases", 
+	plugin = { "html:target/cucumber/wikipedia.html"}, 
+	monochrome=true, 
+	snippets = SnippetType.CAMELCASE, 
+	glue = { "dtu.planning.acceptance_tests"})
 public class AcceptanceTest {
 
 }
