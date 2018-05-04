40ff82866200d70c404698e633d0ef76f481b888
Linnea <linneahj@gmail.com>
2018-05-04 12:26:59 +0200

    Search project by ID method added

--- 02161_Exam_project/src/dtu/planning/app/PlanningApp.java
+++ 02161_Exam_project/src/dtu/planning/app/PlanningApp.java
@@ -13,7 +13,7 @@ import javax.swing.plaf.synth.SynthSpinnerUI;
 
 
 public class PlanningApp {
-	
+
 	private boolean adminLoggedIn = false;
 	private List<Project> currentProjects = new ArrayList<>();
 	private List<Employee> currentEmployees = new ArrayList<>();
@@ -27,8 +27,8 @@ public class PlanningApp {
 	public List<Project> getProjects() {
 		return Collections.unmodifiableList(currentProjects);
 	}
-	
-	
+
+
 	public boolean adminLoggedIn() {
 		return adminLoggedIn;
 	}
@@ -40,14 +40,14 @@ public class PlanningApp {
 
 	public void adminLogOut() {
 		adminLoggedIn = false;
-		
+
 	}
 
-	
-	
 
-	
-	
+
+
+
+
 	/**
 	 * Creates a project provided the administrator is logged in
 	 *  
@@ -56,52 +56,52 @@ public class PlanningApp {
 	 */
 	public void createProject(Project project) throws OperationNotAllowedException {
 		checkAdministratorLoggedIn();
-		
+
 		checkNameIsAvailable(project);
-		
+
 		currentProjects.add(project);
 	}
-	
+
 	private void checkAdministratorLoggedIn() throws OperationNotAllowedException {
 		if (!adminLoggedIn()) {
 			throw new OperationNotAllowedException("Administrator login required");
 		} 
 	}
-	
+
 	private void checkNameIsAvailable(Project project) throws OperationNotAllowedException {
 
 		List<String> projectNames = new ArrayList<>();
 		for (Project p : currentProjects) {
 			projectNames.add(p.getName());
 		}
-		
+
 		if (projectNames.contains(project.getName())) {
 			throw new OperationNotAllowedException("Name for project is already used");
 		}
-		
+
 	} 
-	
+
 	/**
 	 * Removes project
 	 */
 	public void removeProject(Project existingProject) throws OperationNotAllowedException {
 		checkAdministratorLoggedIn();
 		checkProjectExists(existingProject);
-		
+
 		removeActivitiesFromEmployees(existingProject);
 		removeAllEmployeesFromProject(existingProject);
-				
+
 		currentProjects.remove(existingProject);
-		
+
 	}
- 
+
 	private void removeActivitiesFromEmployees(Project existingProject) {
 		for (Activity a: existingProject.getActivities()) {
 			for (Employee e: a.getAssignedEmployees()) {
 				e.unassignActivity(a);
 			}
 		}
-		
+
 	}
 
 
@@ -112,15 +112,15 @@ public class PlanningApp {
 			}
 		}
 	}
-	
+
 	private void checkProjectExists(Project project) throws OperationNotAllowedException {
 		if (!currentProjects.contains(project)) {
 			throw new OperationNotAllowedException("Project does not exist");			
 		}
 	}
 
-	
-	
+
+
 	/**
 	 * Register an employee if the administrator is logged in
 	 *  
@@ -131,28 +131,28 @@ public class PlanningApp {
 		if (adminLoggedIn) {
 			if (currentEmployees.contains(employee)) {
 				throw new Exception("Employee is already registered");
-			
+
 			}
 			if (searchEmployeeID(employee.getID()) != null) {
 				throw new Exception("Employee not registered - ID already used");
-			
+
 			}else {
-			currentEmployees.add(employee);
+				currentEmployees.add(employee);
 			}
 		}
-	
+
 	}	
 
-	
+
 	public Employee searchEmployeeID(String searchID) {
-		 for (Employee employee : currentEmployees ) {
-			 if (employee.match(searchID)) {
-			 return employee;
-			 }
-		 }
+		for (Employee employee : currentEmployees ) {
+			if (employee.match(searchID)) {
+				return employee;
+			}
+		}
 		return null;
 	}
-	
+
 
 	/**
 	 * @return the list of employees currently registered in the system.
@@ -160,7 +160,7 @@ public class PlanningApp {
 	public List<Employee> getEmployees() {
 		return Collections.unmodifiableList(currentEmployees);
 	}
-	
+
 
 	public void removeEmployee(Employee employee) throws Exception {
 		if (adminLoggedIn) {
@@ -177,21 +177,21 @@ public class PlanningApp {
 		}
 
 	}
-	
+
 	public Calendar yearWeekParser(String str) throws ParseException {
 		DateFormat format = new SimpleDateFormat("yyyy-ww");
 		Date date = format.parse(str);
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(date);
-		
+
 		if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
 			calendar.add(Calendar.DATE, 1);
 		}
-		
+
 		return calendar;
 	}
 
-	
+
 	/**
 	 * Gets list of available employees not working in specific weeks
 	 *  
@@ -201,54 +201,64 @@ public class PlanningApp {
 	 * 
 	 */
 	public void getAvailableEmployeesInWeek(Calendar startweek, Calendar endweek) throws Exception {
-			for (Employee employee : currentEmployees) {
-				if (!employee.isEmployeeAvailable(startweek, endweek)) availableEmployees.add(employee);
-			}
-			getAvailableEmployees();
+		for (Employee employee : currentEmployees) {
+			if (!employee.isEmployeeAvailable(startweek, endweek)) availableEmployees.add(employee);
+		}
+		getAvailableEmployees();
 
 	}
-	
-	
+
+
 
 	public List<Employee> getAvailableEmployees() {
 		return Collections.unmodifiableList(availableEmployees);
 	}
-	
 
-	
+
+
 	public Employee getcurrentUser() {
-	    if (currentUser == null) {
-	        return null;
-	    }
-	    return currentUser;
+		if (currentUser == null) {
+			return null;
+		}
+		return currentUser;
 	}
 
 
 	public void userLogin(String employeeID) {
-	    if ( searchEmployeeID(employeeID)!=null) {
-	        setCurrentUser( searchEmployeeID(employeeID));
-	    }
-	    
+		if ( searchEmployeeID(employeeID)!=null) {
+			setCurrentUser( searchEmployeeID(employeeID));
+		}
+
 	}
 
 	public void logOut() {
-	    currentUser = null;		
+		currentUser = null;		
 	}
 
 
 
 	public void setCurrentUser(Employee employee) {
-	    currentUser = employee;
-	 
+		currentUser = employee;
+
+	}
+
+	public boolean isUserTeamleader() {
+		for (Project project : currentProjects ) {
+			if (project.getTeamleader().equals(currentUser)){
+				return true;
+			}
+		}
+		return false;
 	}
 	
-	public boolean isUserTeamleader() {
-	    for (Project project : currentProjects ) {
-	        if (project.getTeamleader().equals(currentUser)){
-	            return true;
-	            }
-	         }
-	        return false;
-	        }
-	
+	public Project getExistingProjectByProjectNumber(String projectNumber) throws Exception {
+		for (Project p: currentProjects) {
+			if (p.getProjectNumber().equals(projectNumber)) {
+				return p;
+			}
+		}
+		
+		throw new Exception("There are no current projects with project number " + projectNumber);
+	}
+
 }
