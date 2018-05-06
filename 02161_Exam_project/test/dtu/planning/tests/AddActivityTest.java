161873a6603047f0bcb4b38e29648bcd6206fa93
lukage1709 <34738620+lukage1709@users.noreply.github.com>
2018-05-06 19:00:49 +0200

    minor change to addActivity junit test

--- 02161_Exam_project/test/dtu/planning/tests/AddActivityTest.java
+++ 02161_Exam_project/test/dtu/planning/tests/AddActivityTest.java
@@ -26,17 +26,24 @@ public class AddActivityTest {
 	Activity activity1;
 	Activity activity2;
 	Project newProject;
-
+	Employee employee;
+	
 
 	@Test (expected = OperationNotAllowedException.class)
 	public void testInputDataSetA() throws  ParseException, OperationNotAllowedException {
+		planningApp.adminLogin("admin1234");
 		newProject = new Project("TestProjectA", PlanningApp.yearWeekParser("2018-03"));
-		// List<Project> projectList = new ArrayList<Project>();
-		// projectList.add(new Project("TestProjectA", PlanningApp.yearWeekParser("2018-03")));
-		Activity activity1 = new Activity(newProject.getActivityIdCounter(), "name", 0, planningApp.yearWeekParser("2018-1"), planningApp.yearWeekParser("2018-3"));
-		Activity activity2 = new Activity(newProject.getActivityIdCounter(), "name", 0, planningApp.yearWeekParser("2018-1"), planningApp.yearWeekParser("2018-3"));
-		newProject.addActivity(activity1);
+		Employee employee = new Employee("employeeID", "employeeName");
+			
+		
+		
+		Activity activity1 = new Activity(newProject.getActivityIdCounter(), "name", 0, PlanningApp.yearWeekParser("2018-3"), PlanningApp.yearWeekParser("2018-3"));
+		Activity activity2 = new Activity(newProject.getActivityIdCounter(), "name", 0, PlanningApp.yearWeekParser("2018-3"), PlanningApp.yearWeekParser("2018-4"));
+		
 		newProject.addActivity(activity2);
+		newProject.addActivity(activity1);
+		
+		
 		
 	}
 	
@@ -44,10 +51,9 @@ public class AddActivityTest {
 	@Test (expected = OperationNotAllowedException.class)
 	public void testInputDataSetB() throws  ParseException, OperationNotAllowedException {
 		newProject = new Project("TestProjectA", PlanningApp.yearWeekParser("2018-03"));
-		// List<Project> projectList = new ArrayList<Project>();
-		// projectList.add(new Project("TestProjectA", PlanningApp.yearWeekParser("2018-03")));
+		
 		Activity activity1 = new Activity(newProject.getActivityIdCounter(), "name1", 0, planningApp.yearWeekParser("2018-3"), planningApp.yearWeekParser("2018-1"));
-		Activity activity2 = new Activity(newProject.getActivityIdCounter(), "name2", 0, planningApp.yearWeekParser("2018-1"), planningApp.yearWeekParser("2018-3"));
+		Activity activity2 = new Activity(newProject.getActivityIdCounter(), "name2", 0, planningApp.yearWeekParser("2018-3"), planningApp.yearWeekParser("2018-4"));
 		newProject.addActivity(activity2);
 		newProject.addActivity(activity1);
 		
@@ -57,10 +63,9 @@ public class AddActivityTest {
 	@Test (expected = OperationNotAllowedException.class)
 	public void testInputDataSetC() throws  ParseException, OperationNotAllowedException {
 		newProject = new Project("TestProjectA", PlanningApp.yearWeekParser("2018-15"));
-		// List<Project> projectList = new ArrayList<Project>();
-		// projectList.add(new Project("TestProjectA", PlanningApp.yearWeekParser("2018-03")));
+		
 		Activity activity1 = new Activity(newProject.getActivityIdCounter(), "name1", 0, planningApp.yearWeekParser("2018-10"), planningApp.yearWeekParser("2018-20"));
-		Activity activity2 = new Activity(newProject.getActivityIdCounter(), "name2", 0, planningApp.yearWeekParser("2018-1"), planningApp.yearWeekParser("2018-3"));
+		Activity activity2 = new Activity(newProject.getActivityIdCounter(), "name2", 0, planningApp.yearWeekParser("2018-15"), planningApp.yearWeekParser("2018-20"));
 		newProject.addActivity(activity2);
 		newProject.addActivity(activity1);
 		
@@ -71,8 +76,7 @@ public class AddActivityTest {
 	@Test 
 	public void testInputDataSetD() throws  ParseException, OperationNotAllowedException {
 		newProject = new Project("TestProjectA", PlanningApp.yearWeekParser("2018-1"));
-		// List<Project> projectList = new ArrayList<Project>();
-		// projectList.add(new Project("TestProjectA", PlanningApp.yearWeekParser("2018-03")));
+		
 		Activity activity1 = new Activity(newProject.getActivityIdCounter(), "name1", 0, planningApp.yearWeekParser("2018-1"), planningApp.yearWeekParser("2018-3"));
 		Activity activity2 = new Activity(newProject.getActivityIdCounter(), "name2", 0, planningApp.yearWeekParser("2018-2"), planningApp.yearWeekParser("2018-3"));
 		newProject.addActivity(activity1);
