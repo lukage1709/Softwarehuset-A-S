a6e3ebdcc5dc0228459547dc881a1bf0f71d5611
Linnea <linneahj@gmail.com>
2018-05-06 18:43:24 +0200

    refactoring

--- 02161_Exam_project/src/dtu/planning/app/PlanningApp.java
+++ 02161_Exam_project/src/dtu/planning/app/PlanningApp.java
@@ -27,12 +27,29 @@ public class PlanningApp {
 		return Collections.unmodifiableList(currentProjects);
 	}
 
+	// Returns current user and null if none
+	public Employee getcurrentUser() {
+		return currentUser;
+	}
+
+	// Sets current user to employee
+	public void setCurrentUser(Employee employee) {
+		currentUser = employee;
+	}
+
 
 	/**
 	 * @return whether or not the administrator is logged in
 	 */
 	public boolean adminLoggedIn() {
 		return adminLoggedIn;
+	}
+
+	/**
+	 * @return the list of employees currently registered in the system.
+	 */
+	public List<Employee> getEmployees() {
+		return Collections.unmodifiableList(currentEmployees);
 	}
 
 	/**
@@ -53,16 +70,13 @@ public class PlanningApp {
 	}
 
 
-	/**
-	 * Creates a project provided preconditions are 
-	 *  
-	 *  @param project to be created
-	 *  
-	 *  @pre project != null
-	 *  @pre admin logged in
-	 *  @pre project name is unique
-	 *  @post currentProject.size() == currentProject@pre.size().plus(project)
-	 */
+
+	// Creates a project provided preconditions are 
+	// @param project to be created
+	// @pre project != null
+	// @pre admin logged in
+	// @pre project name is unique
+	// @post currentProject.size() == currentProject@pre.size().plus(project)
 	public void createProject(Project project) throws OperationNotAllowedException {
 		List<Project> projectsAtPre = new ArrayList<>(getProjects()); // To be used for postcondition
 
@@ -79,14 +93,15 @@ public class PlanningApp {
 		assert projectsAtPre.equals(currentProjects);
 	}
 
+	// Throws exception if administrator is not logged in, otherwise no change of state
 	private void checkAdministratorLoggedIn() throws OperationNotAllowedException {
 		if (!adminLoggedIn()) {
 			throw new OperationNotAllowedException("Administrator login required");
 		} 
 	}
 
+	// Throws exception if the name of the project is already in use, otherwise no change of state
 	private void checkNameIsAvailable(Project project) throws OperationNotAllowedException {
-
 		List<String> projectNames = new ArrayList<>();
 		for (Project p : currentProjects) {
 			projectNames.add(p.getName());
@@ -95,12 +110,9 @@ public class PlanningApp {
 		if (projectNames.contains(project.getName())) {
 			throw new OperationNotAllowedException("Name for project is already used");
 		}
-
 	} 
 
-	/**
-	 * Removes project
-	 */
+	// Removes project
 	public void removeProject(Project existingProject) throws OperationNotAllowedException {
 		checkAdministratorLoggedIn();
 		checkProjectExists(existingProject);
@@ -112,16 +124,16 @@ public class PlanningApp {
 
 	}
 
+	// Removes activities the employees' lists of current activities they are assigned to 
 	private void removeActivitiesFromEmployees(Project existingProject) {
 		for (Activity a: existingProject.getActivities()) {
 			for (Employee e: a.getAssignedEmployees()) {
 				e.unassignActivity(a);
 			}
 		}
-
 	}
 
-
+	// Removes all employees for all acitivites in a project
 	private void removeAllEmployeesFromProject(Project project) throws OperationNotAllowedException {
 		if (project.getActivities() != null) {
 			for (Activity a: project.getActivities()) {
@@ -130,36 +142,42 @@ public class PlanningApp {
 		}
 	}
 
+	// Throw exception is @param project is not in the list of current projects
 	private void checkProjectExists(Project project) throws OperationNotAllowedException {
 		if (!currentProjects.contains(project)) {
 			throw new OperationNotAllowedException("Project does not exist");			
 		}
 	}
 
-
-
-	/**
-	 * Register an employee if the administrator is logged in
-	 *  
-	 * @param employee to be registered
-	 * 
-	 */
+	// Register an employee if the administrator is logged in
+	// @param employee to be registered
 	public void registerEmployee(Employee employee) throws OperationNotAllowedException {
 		checkAdministratorLoggedIn();
-		if (currentEmployees.contains(employee)) {
-			throw new OperationNotAllowedException("Employee is already registered");
 
-		}
-		if (searchEmployeeID(employee.getID()) != null) {
-			throw new OperationNotAllowedException("Employee not registered - ID already used");
+		checkEmployeeNotRegistered(employee);
 
-		}else {
-			currentEmployees.add(employee);
-		}
+		checkIdAvailable(employee);
 
+		currentEmployees.add(employee);
 	}	
 
+	// Throws exception if employee is already registered
+	private void checkEmployeeNotRegistered(Employee employee) throws OperationNotAllowedException {
+		if (currentEmployees.contains(employee)) {
+			throw new OperationNotAllowedException("Employee is already registered");
+		}
+	}
 
+	// Throws exception if id of employee is already used
+	private void checkIdAvailable(Employee employee) throws OperationNotAllowedException {
+		if (searchEmployeeID(employee.getID()) != null) {
+			throw new OperationNotAllowedException("Employee not registered - ID already used");
+		}
+	}
+
+	// Search through list of current employees to find one with matching id
+	// @param id to match
+	// @return registered employee with corresonding id
 	public Employee searchEmployeeID(String searchID) {
 		for (Employee employee : currentEmployees ) {
 			if (employee.match(searchID)) {
@@ -169,27 +187,21 @@ public class PlanningApp {
 		return null;
 	}
 
-
-	/**
-	 * @return the list of employees currently registered in the system.
-	 */
-	public List<Employee> getEmployees() {
-		return Collections.unmodifiableList(currentEmployees);
-	}
-
-
 	public void removeEmployee(Employee employee) throws OperationNotAllowedException {
 		checkAdministratorLoggedIn();
 
+		checkEmployeeExists(employee);
+
 		assert employee != null;
 
+		currentEmployees.remove(employee);
+	}
+
+	// Throws exception employee is not registered
+	private void checkEmployeeExists(Employee employee) throws OperationNotAllowedException {
 		if (searchEmployeeID(employee.getID()) == null) {
 			throw new OperationNotAllowedException("Employee does not exists in list of employees");
-
-		} else {
-			currentEmployees.remove(employee);
 		}
-
 	}
 
 	public static Calendar yearWeekParser(String str) throws ParseException {
@@ -198,65 +210,46 @@ public class PlanningApp {
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(date);
 
-		if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
+		if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) { // This only gets called if computer sets sunday as first weekday
 			calendar.add(Calendar.DATE, 1);
 		}
 
 		return calendar;
 	}
-	
+
 	public static String yearWeekFormat(Calendar cal) {
 		SimpleDateFormat format1 = new SimpleDateFormat("yyyy-ww");
 		String formatted = format1.format(cal.getTime());
 		return formatted;
 	}
 
-
-	/**
-	 * Gets list of available employees not working in specific weeks
-	 *  
-	 * @param start and end dates 
-	 * @return 
-	 * @throws Exception 
-	 * 
-	 */
+	// Gets list of available employees not working in specific weeks
+	// @param start and end dates 
+	// @return list of employees available between start- and enddate
 	public List<Employee> getAvailableEmployeesInWeek(Calendar startweek, Calendar endweek) {
 		List<Employee> availableEmployees = new ArrayList<>();
+
 		for (Employee employee : currentEmployees) {
 			if (employee.isEmployeeAvailable(startweek, endweek)) availableEmployees.add(employee);
 		}
+
 		return availableEmployees;
-
 	}
 
-
-
-	public Employee getcurrentUser() {
-		if (currentUser == null) {
-			return null;
+	// Logs in user if user exist
+	public void userLogin(String employeeId) {
+		// checkEmployeeExist(employeeId);
+		if (searchEmployeeID(employeeId) != null) {
+			setCurrentUser(searchEmployeeID(employeeId));
 		}
-		return currentUser;
 	}
 
-
-	public void userLogin(String employeeID) {
-		if ( searchEmployeeID(employeeID)!=null) {
-			setCurrentUser( searchEmployeeID(employeeID));
-		}
-
-	}
-
+	// Logs out current user
 	public void logOut() {
 		currentUser = null;		
 	}
 
-
-
-	public void setCurrentUser(Employee employee) {
-		currentUser = employee;
-
-	}
-
+	// Returns true if the current user is teamleader, otherwise false
 	public boolean isUserTeamleader() {
 		for (Project project : currentProjects ) {
 			if (project.getTeamleader() != null && project.getTeamleader().equals(currentUser)){
@@ -266,6 +259,7 @@ public class PlanningApp {
 		return false;
 	}
 
+	// Returns an existing project that matches given projectnumber. If none exist, then return null
 	public Project getExistingProjectByProjectNumber(String projectNumber) {
 		for (Project p: currentProjects) {
 			if (p.getProjectNumber().equals(projectNumber)) {
