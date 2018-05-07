cc0689e024a8b850d31c6bec37dfadcac48b00cc
s144025 <9057612+s144025@users.noreply.github.com>
2018-05-07 14:16:08 +0200

    minor fixes

--- 02161_Exam_project/src/dtu/planning/ui/WelcomeScreen.java
+++ 02161_Exam_project/src/dtu/planning/ui/WelcomeScreen.java
@@ -16,7 +16,6 @@ public class WelcomeScreen extends Screen {
 		out.println("0: Exit");
 		out.println("1: Employee login");
 		out.println("2: Administrator login");
-		out.println("3: Load test data");
 		out.println("\nSelect option: ");
 	}
 	
@@ -29,34 +28,8 @@ public class WelcomeScreen extends Screen {
 			planningUI.setScreen(new EmployeeLoginScreen());
 		} else if (input.equals("2")) {
 			planningUI.setScreen(new AdminLoginScreen());
-		} else if (input.equals("3")) {
-			loadTestData();
 		} else {
 			planningUI.setScreen(new WelcomeScreen());
 		}
-	}
-	
-	public void loadTestData() {
-		planningUI.getPlanningApp().adminLogin("admin1234");
-		try {
-			Employee employee1 = new Employee("anje", "Anders Jensen"); System.out.println("Added employee \"anje\"");
-			Employee employee2 = new Employee("heha", "Henning Hansen"); System.out.println("Added employee \"heha\"");
-			planningUI.getPlanningApp().registerEmployee(employee1);
-			planningUI.getPlanningApp().registerEmployee(employee2);
-			Project project1 = new Project("Test 1", planningUI.getPlanningApp().yearWeekParser("2018-03")); System.out.println("Added project \"Test 1\"");
-			Project project2 = new Project("Test 2", planningUI.getPlanningApp().yearWeekParser("2018-04"));System.out.println("Added project \"Test 2\"");
-			planningUI.getPlanningApp().createProject(project1);
-			planningUI.getPlanningApp().createProject(project2);
-			
-			project1.assignTeamleader(employee1); System.out.println("Assigned \"anje\" as team leader on project \"Test 1\"");
-		} catch (ParseException e) {
-			e.printStackTrace();
-		} catch (OperationNotAllowedException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		planningUI.getPlanningApp().adminLogOut();
-		
 	}
 }
